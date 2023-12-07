ea851f6db03907555649362c6cb7553891a258b2
ahmadumar559 <ahmadumar559@gmail.com>
2023-12-07 08:23:39 +0700

    ignore config

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/template_payroll';
+$config['base_url'] = 'http://localhost/playground/template_payroll';
 
 /*
 |--------------------------------------------------------------------------
